b14c4fc21b952682755f9598bb426f9d30e4cbed
Dima <dimabojko93@gmail.com>
2023-02-16 17:13:24 +0200

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 
 import Counter from "./components/counter/counter";
+import Card from "./components/card/card";
 
 const App = () => {
-    return <Counter></Counter>
+    return (
+        <div>
+            <Counter></Counter> 
+            <Card 
+                urlCard="img/card-photo.jpg"
+                urlAuthorImg = "img/card-author.png" 
+                urlRating = "img/card-rating.png" 
+                urlStudents = "img/card-students.png"
+                urlModules = "img/card-modules.png"
+                urlDuration = "img/card-duration.png"
+                title = "UX Design : How To Implement Usability Testing"
+                author = "Alfredo Rhiel Madsen"
+                rating = "4.5"
+                students = "500 Student"
+                level = "Beginer"
+                modules = "5 Modul"
+                duration = "1h 30m"
+            >
+            </Card>  
+        </div>   
+    )    
 }
 
 export default App;
---
+++ src/components/card/card.js
@@ -0,0 +1,53 @@
+import "./card.css";
+
+const Card = (props) => {
+
+    const 
+    {urlCard, 
+     urlAuthorImg, 
+     urlRating, 
+     urlStudents, 
+     urlModules, 
+     urlDuration, 
+     title, 
+     author, 
+     rating,
+     students, 
+     level, 
+     modules,
+     duration, 
+     } = props;
+
+    return (
+        <div className="card">
+            <img className="card-img" src={urlCard} alt="img" width= "294" height= "150"></img>
+            <h2 className="card-headline">{title}</h2>
+            <div className="card-item author">
+                <img src={urlAuthorImg} alt="img-author" width= "30" height= "30"/>
+                <span className="author-name">{author}</span>
+            </div>
+            <div className="card-item rating">
+                <img src={urlRating} alt="img-rating" width= "15" height= "15"/>
+                <span className="author-name">{rating}</span>
+            </div>
+            <div className="card-item students">
+                <img src={urlStudents} alt="img-students" width= "16" height= "16"/>
+                <span className="author-name">{students}</span>
+            </div>
+            <div className="card-item modules">
+                <img src={urlModules} alt="img-modules" width= "16" height= "16"/>
+                <span className="author-name">{modules}</span>
+            </div>
+            <div className="card-item duration">
+                <img src={urlDuration} alt="img-duration" width= "16" height= "16"/>
+                <span className="author-name">{duration}</span>
+            </div>
+            <div className="level">
+                <span className="author-name">{level}</span>
+            </div>
+        </div>
+    )
+
+}
+
+export default Card;
